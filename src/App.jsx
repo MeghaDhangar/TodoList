21a16749a30925455c18c09f9c7aa4c68bbca361
Megha Dhangar <meghadhangar1301@gmail.com>
2024-05-04 13:58:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import TodoList from './Components/TodoList'
+
+function App() {
+  return (
+    <>
+    <TodoList/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/TodoList.jsx
@@ -0,0 +1,11 @@
+
+
+function TodoList() {
+  return (
+    <div>
+      <h1>hello megha dhangar</h1>
+    </div>
+  )
+}
+
+export default TodoList
